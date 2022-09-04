4dcbca2fadd93cfdebbfaaaa5c87b861f6bce0df
NotArandomGUY <hhin0001@gmail.com>
2022-09-04 20:23:35 +0800

    Update RunConvert.bat

--- RunConvert.bat
+++ RunConvert.bat
@@ -1,7 +1,36 @@
 @echo off
+chcp 65001
 
-set /p ver=Game Version:
-set /p filter=Filter:
+:start
+cls
+
+echo Game version
+echo   Example: 3.0.0
+echo.
+set /p ver=::
+cls
+
+echo Output filter
+echo   Type -1 to list filter ids
+echo   Leave it blank to generate all files
+echo.
+set /p filter=::
+cls
+
+setlocal enableextensions
+if not exist ".\InputData\%ver%" (
+  md ".\InputData\%ver%"
+  echo Put your resources in ".\InputData\%ver%" then press enter to continue.
+  echo File structure:
+  echo   .\InputData\%ver%
+  echo   ├─BinOutput
+  echo   ├─ExcelBinOutput
+  echo   └─Scripts
+  pause >nul
+  cls
+)
+endlocal
 
 node ./build -mode:3 -ver:%ver% -filter:%filter%
 pause
+goto start
